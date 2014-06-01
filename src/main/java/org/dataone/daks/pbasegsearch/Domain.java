d7739be8a9fe4e5ccf77942323b25af74990312d
victorcuevasv <victorcuevasv@gmail.com>
2014-05-31 23:03:18 -0700

    Created domain descriptions.

---
+++ src/main/java/org/dataone/daks/pbasegsearch/Domain.java
@@ -0,0 +1,75 @@
+package org.dataone.daks.pbasegsearch;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+import org.dom4j.Element;
+
+
+public class Domain {
+	
+	
+	private List<String> termsList;
+	private String name;
+	private Random rand;
+	
+	
+	public Domain() {
+		this.rand = new Random();
+	}
+	
+	
+	public String init(Element domainElement) {
+		this.termsList = new ArrayList<String>();
+		Element elemName = domainElement.element("name");
+		String name = elemName.getText();
+		Element elemTerms = domainElement.element("terms");
+		String termsStr = elemTerms.getText();
+		StringTokenizer tokenizer = new StringTokenizer(termsStr);
+		while( tokenizer.hasMoreTokens() ) {
+			this.termsList.add(tokenizer.nextToken());
+		}
+		this.name = name;
+		return name;
+	}
+	
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	
+	private String getRandomTerm() {
+		int randPosition = this.randInt(0, this.termsList.size()-1);
+		String term = this.termsList.get(randPosition);
+		return term;
+	}
+	
+	
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		String newLine = System.getProperty("line.separator");
+		buffer.append("Name: " + this.name + newLine);
+		for( int i = 0; i < this.termsList.size(); i++ ) {
+			buffer.append(this.termsList.get(i) + " ");
+			if( (i+1) % 5 == 0 )
+				buffer.append(newLine);
+		}
+		return buffer.toString();
+	}
+	
+	
+    private int randInt(int min, int max) {
+	    int randomNum = this.rand.nextInt((max - min) + 1) + min;
+	    return randomNum;
+	}
+	
+
+}
+
+
+
+
+
---
+++ src/main/java/org/dataone/daks/pbasegsearch/DomainCatalog.java
@@ -0,0 +1,117 @@
+package org.dataone.daks.pbasegsearch;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+public class DomainCatalog {
+
+	
+	List<String> genericNames;
+	List<String> specificNames;
+	Hashtable<String, Domain> genericHT;
+	Hashtable<String, Domain> specificHT;
+	private Random rand;
+	
+	
+	public DomainCatalog(String genericFile, String specificFile) {
+		this.rand = new Random();
+		this.genericNames = new ArrayList<String>();
+		this.specificNames = new ArrayList<String>();
+		this.genericHT = new Hashtable<String, Domain>();
+		this.specificHT = new Hashtable<String, Domain>();
+		this.init(genericFile, true);
+		this.init(specificFile, false);
+	}
+	
+	
+	public static void main(String args[]) {
+		DomainCatalog catalog = new DomainCatalog(args[0], args[1]);
+		System.out.println(catalog.toString());
+	}
+	
+	
+	private void init(String xmlFileName, boolean isGeneric) {
+		Document document = this.getDocument(xmlFileName);
+		Element root = document.getRootElement();
+		for ( Iterator<Element> i = root.elementIterator("domain"); i.hasNext(); ) {
+			Element domainElem = i.next();
+			Domain domain = new Domain();
+			String name = domain.init(domainElem);
+			if( isGeneric ) {
+				this.genericNames.add(name);
+				this.genericHT.put(name, domain);
+			}
+			else {
+				this.specificNames.add(name);
+				this.specificHT.put(name, domain);
+			}
+		}
+	}
+	
+	
+	private Domain getRandomGenericDomain() {
+		int randPosition = this.randInt(0, this.genericNames.size()-1);
+		String genericName = this.genericNames.get(randPosition);
+		Domain genericDomain = this.genericHT.get(genericName);
+		return genericDomain;
+	}
+	
+	
+	private Domain getRandomSpecificDomain() {
+		int randPosition = this.randInt(0, this.specificNames.size()-1);
+		String specificName = this.specificNames.get(randPosition);
+		Domain specificDomain = this.specificHT.get(specificName);
+		return specificDomain;
+	}
+	
+	
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		String newLine = System.getProperty("line.separator");
+		buffer.append("GENERIC DOMAINS: \n");
+		for( String name: this.genericNames ) {
+			Domain domain = this.genericHT.get(name);
+			buffer.append(domain.toString());
+			buffer.append(newLine);
+		}
+		buffer.append("SPECIFIC DOMAINS: \n");
+		for( String name: this.specificNames ) {
+			Domain domain = this.specificHT.get(name);
+			buffer.append(domain.toString());
+			buffer.append(newLine);
+		}
+		return buffer.toString();
+	}
+    
+    
+    private int randInt(int min, int max) {
+	    int randomNum = this.rand.nextInt((max - min) + 1) + min;
+	    return randomNum;
+	}
+    
+    
+    private Document getDocument(String xmlFileName) {
+		Document document = null;
+		SAXReader reader = new SAXReader();
+		try {
+			document = reader.read(xmlFileName);
+		}
+		catch (DocumentException e) {
+			e.printStackTrace();
+		}
+		return document;
+	}
+	
+	
+}
+
+
+

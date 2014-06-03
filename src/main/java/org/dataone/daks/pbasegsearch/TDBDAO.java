a47a9aa0648475d9fa6317a4e783c80156137217
victorcuevasv <victorcuevasv@gmail.com>
2014-06-03 01:20:40 -0700

    Retrieving the graph stored in RDF as JSON.

--- src/main/java/org/dataone/daks/pbasegsearch/TDBDAO.java
+++ src/main/java/org/dataone/daks/pbasegsearch/TDBDAO.java
@@ -1,7 +1,6 @@
 package org.dataone.daks.pbasegsearch;
 
 import java.util.List;
-import java.util.ArrayList;
 
 import com.hp.hpl.jena.query.*;
 import com.hp.hpl.jena.rdf.model.*;
@@ -58,159 +57,122 @@ public class TDBDAO {
 	}
 	
 	
-    public JSONArray getProcesses(String wfID) {
-    	String sparqlQueryString = "PREFIX provone: <http://purl.org/provone/ontology#> \n" +
+	Dataset getDataset() {
+		return this.ds;
+	}
+	
+	
+	public JSONObject getGraph() {
+		JSONObject graphObj = new JSONObject();
+		JSONArray nodesArray = new JSONArray();
+		JSONArray edgesArray = new JSONArray();
+		try {
+			//Get the nodes
+			JSONArray dataNodesArray = this.getNodes("Data");
+			for( int i = 0; i < dataNodesArray.length(); i++ )
+				nodesArray.put(dataNodesArray.get(i));
+			JSONArray workflowNodesArray = this.getNodes("Workflow");
+			for( int i = 0; i < workflowNodesArray.length(); i++ )
+				nodesArray.put(workflowNodesArray.get(i));
+			JSONArray authorNodesArray = this.getNodes("Author");
+			for( int i = 0; i < authorNodesArray.length(); i++ )
+				nodesArray.put(authorNodesArray.get(i));
+			JSONArray publicationNodesArray = this.getNodes("Publication");
+			for( int i = 0; i < publicationNodesArray.length(); i++ )
+				nodesArray.put(publicationNodesArray.get(i));
+			//Get the edges
+			JSONArray wasAttToInvWDEdgesArray = this.getEdges("wasAttToInv", "Workflow", "Data");
+			for( int i = 0; i < wasAttToInvWDEdgesArray.length(); i++ )
+				edgesArray.put(wasAttToInvWDEdgesArray.get(i));
+			JSONArray wasAttToInvAWEdgesArray = this.getEdges("wasAttToInv", "Author", "Workflow");
+			for( int i = 0; i < wasAttToInvAWEdgesArray.length(); i++ )
+				edgesArray.put(wasAttToInvAWEdgesArray.get(i));
+			JSONArray wasAttToInvAPEdgesArray = this.getEdges("wasAttToInv", "Author", "Publication");
+			for( int i = 0; i < wasAttToInvAPEdgesArray.length(); i++ )
+				edgesArray.put(wasAttToInvAPEdgesArray.get(i));
+			JSONArray wasDfInvEdgesArray = this.getEdges("wasDfInv", "Data", "Data");
+			for( int i = 0; i < wasDfInvEdgesArray.length(); i++ )
+				edgesArray.put(wasDfInvEdgesArray.get(i));
+			JSONArray usedEdgesArray = this.getEdges("used", "Publication", "Data");
+			for( int i = 0; i < usedEdgesArray.length(); i++ )
+				edgesArray.put(usedEdgesArray.get(i));
+			//Add nodes and edges to the graph object
+			graphObj.put("nodes", nodesArray);
+			graphObj.put("edges", edgesArray);
+		}
+		catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return graphObj;
+	}
+	
+	
+	private JSONArray getNodes(String className) {
+		String sparqlQueryString = "PREFIX searchont: <http://purl.org/provone/searchontology#> \n" +
         		"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n" +
         		"PREFIX dc: <http://purl.org/dc/terms/> \n" +
         		"PREFIX wfms: <http://www.vistrails.org/registry.xsd#> \n" +
         		"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \n" +
-        		"SELECT ?i ?t ?service ?avgtime ?avgcost ?avgreliability ?wfavgtime ?wfavgcost ?wfavgreliability " + 
-        		"WHERE {  ?process dc:identifier ?i . " +
-        		"?process dc:title ?t . " +
-        		"OPTIONAL { ?process wfms:service ?service } . " +
-        		"OPTIONAL { ?process wfms:avgtime ?avgtime } . " +
-        		"OPTIONAL { ?process wfms:avgcost ?avgcost } . " +
-        		"OPTIONAL { ?process wfms:avgreliability ?avgreliability } . " +
-        		"OPTIONAL { ?process wfms:wfavgtime ?wfavgtime } . " +
-        		"OPTIONAL { ?process wfms:wfavgcost ?wfavgcost } . " +
-        		"OPTIONAL { ?process wfms:wfavgreliability ?wfavgreliability } . " +
-        		"?process rdf:type provone:Process . " + 
-        		"?wf rdf:type provone:Workflow . " +
-        		"?wf dc:identifier " + "\"" + wfID + "\"^^xsd:string . " +
-        		"?wf provone:hasSubProcess ?process . }";
+        		"SELECT ?id ?title " + 
+        		"WHERE {  ?data dc:identifier ?id . " +
+        		"?data dc:title ?title . " +
+        		"?data rdf:type searchont:" + className + " . }";
+		JSONArray jsonArray = new JSONArray();
         Query query = QueryFactory.create(sparqlQueryString);
         QueryExecution qexec = QueryExecutionFactory.create(query, this.ds);
         ResultSet results = qexec.execSelect();
-        JSONArray nodesArray = new JSONArray();
-        try {
-        	for ( ; results.hasNext() ; ) {
-        		JSONObject nodeObj = new JSONObject();
-        		QuerySolution soln = results.nextSolution();
-        		String id = soln.getLiteral("i").getString();
-				nodeObj.put("nodeId", id);
-				String title = soln.getLiteral("t").getString();
-	            nodeObj.put("title", title);
-	            Literal serviceLit = soln.getLiteral("service");
-	            if( serviceLit != null )
-	            	nodeObj.put("service", serviceLit.getString());
-	            Literal avgtimeLit = soln.getLiteral("avgtime");
-	            if( avgtimeLit != null ) {
-	            	double avgtime = avgtimeLit.getDouble();
-	            	String avgtimeStr = String.format("%.3f", avgtime);
-	            	nodeObj.put("avgtime", avgtimeStr);
-	            }
-	            Literal avgcostLit = soln.getLiteral("avgcost");
-	            if( avgcostLit != null ) {
-	            	double avgcost = avgcostLit.getDouble();
-	            	String avgcostStr = String.format("%.3f", avgcost);
-	            	nodeObj.put("avgcost", avgcostStr);
-	            }
-	            Literal avgreliabilityLit = soln.getLiteral("avgreliability");
-	            if( avgreliabilityLit != null ) {
-	            	double avgreliability = avgreliabilityLit.getDouble();
-	            	String avgreliabilityStr = String.format("%.3f", avgreliability);
-	            	nodeObj.put("avgrebty", avgreliabilityStr);
-	            }
-	            Literal wfavgtimeLit = soln.getLiteral("wfavgtime");
-	            if( wfavgtimeLit != null ) {
-	            	double wfavgtime = wfavgtimeLit.getDouble();
-	            	String wfavgtimeStr = String.format("%.3f", wfavgtime);
-	            	nodeObj.put("wfavgtime", wfavgtimeStr);
-	            }
-	            Literal wfavgcostLit = soln.getLiteral("wfavgcost");
-	            if( wfavgcostLit != null ) {
-	            	double wfavgcost = wfavgcostLit.getDouble();
-	            	String wfavgcostStr = String.format("%.3f", wfavgcost);
-	            	nodeObj.put("wfavgcost", wfavgcostStr);
-	            }
-	            Literal wfavgreliabilityLit = soln.getLiteral("wfavgreliability");
-	            if( wfavgreliabilityLit != null ) {
-	            	double wfavgreliability = wfavgreliabilityLit.getDouble();
-	            	String wfavgreliabilityStr = String.format("%.3f", wfavgreliability);
-	            	nodeObj.put("wfavgrebty", wfavgreliabilityStr);
-	            }
-	            nodesArray.put(nodeObj);
+        for ( ; results.hasNext() ; ) {
+            QuerySolution soln = results.nextSolution();
+            JSONObject jsonObj = new JSONObject();
+            String id = soln.getLiteral("id").getString();
+            String title = soln.getLiteral("title").getString();
+            try {
+            	jsonObj.put("nodeId", id);
+				jsonObj.put("title", title);
+				jsonArray.put(jsonObj);
+			}
+            catch (JSONException e) {
+				e.printStackTrace();
 			}
         }
-        catch (JSONException e) {
-			e.printStackTrace();
-		}
         qexec.close();
-        return nodesArray;
-    }
-	
-	
-	public List<JSONObject> getWasGenByEdges(String wfID, String runID) throws JSONException {
-		String sparqlQueryString = "PREFIX provone: <http://purl.org/provone/ontology#> \n" +
-        		"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n" +
-        		"PREFIX dc: <http://purl.org/dc/terms/> \n" +
-        		"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \n" +
-        		"PREFIX prov: <http://www.w3.org/ns/prov#> \n" +
-				"SELECT ?data_id ?pexec_id WHERE {  " + 
-        		"?wfpexec prov:wasAssociatedWith ?wf . " +
-        		"?wf rdf:type provone:Workflow . " +
-        		"?wf dc:identifier " + "\"" + wfID + "\"^^xsd:string . " +
-        		"?wfpexec dc:identifier " + "\"" + runID + "\"^^xsd:string . " +
-        		"?pexec provone:isPartOf ?wfpexec . " +
-        		"?data rdf:type provone:Data . " +
-        		"?data prov:wasGeneratedBy ?pexec . " +
-        		"?data dc:identifier ?data_id . " +
-        		"?pexec dc:identifier ?pexec_id . " +
-        		"}";
-		List<JSONObject> edgesList = new ArrayList<JSONObject>();
-        Query query = QueryFactory.create(sparqlQueryString);
-        QueryExecution qexec = QueryExecutionFactory.create(query, this.ds);
-        ResultSet results = qexec.execSelect();
-        for ( ; results.hasNext() ; ) {
-            QuerySolution soln = results.nextSolution();
-            JSONObject jsonObj = new JSONObject();
-            String dataId = soln.getLiteral("data_id").getString();
-            String pexecId = soln.getLiteral("pexec_id").getString();
-            String label = "wasGenBy";
-            jsonObj.put("startNodeId", dataId);
-            jsonObj.put("endNodeId", pexecId);
-            jsonObj.put("edgeLabel", label);
-            edgesList.add(jsonObj);
-        }
-        qexec.close();
-		return edgesList;
+		return jsonArray;
 	}
+		
 	
-	
-	public List<JSONObject> getUsedEdges(String wfID, String runID) throws JSONException {
-		String sparqlQueryString = "PREFIX provone: <http://purl.org/provone/ontology#> \n" +
+	private JSONArray getEdges(String objPropName, String sourceClassName, String destClassName) {
+		String sparqlQueryString = "PREFIX searchont: <http://purl.org/provone/searchontology#> \n" +
         		"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n" +
         		"PREFIX dc: <http://purl.org/dc/terms/> \n" +
         		"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \n" +
-        		"PREFIX prov: <http://www.w3.org/ns/prov#> \n" +
-				"SELECT ?data_id ?pexec_id WHERE {  " + 
-        		"?wfpexec prov:wasAssociatedWith ?wf . " +
-        		"?wf rdf:type provone:Workflow . " +
-        		"?wf dc:identifier " + "\"" + wfID + "\"^^xsd:string . " +
-        		"?wfpexec dc:identifier " + "\"" + runID + "\"^^xsd:string . " +
-        		"?pexec provone:isPartOf ?wfpexec . " +
-        		"?data rdf:type provone:Data . " +
-        		"?pexec prov:used ?data . " +
-        		"?data dc:identifier ?data_id . " +
-        		"?pexec dc:identifier ?pexec_id . " +
+				"SELECT ?source_id ?dest_id WHERE {  " + 
+        		"?source searchont:" + objPropName + " ?dest . " +
+        		"?source rdf:type searchont:" + sourceClassName + " . " +
+        		"?source dc:identifier ?source_id . " +
+        		"?dest rdf:type searchont:" + destClassName + " . " +
+        		"?dest dc:identifier ?dest_id . " +
         		"}";
-		List<JSONObject> edgesList = new ArrayList<JSONObject>();
+		JSONArray edgesArray = new JSONArray();
         Query query = QueryFactory.create(sparqlQueryString);
         QueryExecution qexec = QueryExecutionFactory.create(query, this.ds);
         ResultSet results = qexec.execSelect();
         for ( ; results.hasNext() ; ) {
             QuerySolution soln = results.nextSolution();
             JSONObject jsonObj = new JSONObject();
-            String dataId = soln.getLiteral("data_id").getString();
-            String pexecId = soln.getLiteral("pexec_id").getString();
-            String label = "used";
-            jsonObj.put("startNodeId", pexecId);
-            jsonObj.put("endNodeId", dataId);
-            jsonObj.put("edgeLabel", label);
-            edgesList.add(jsonObj);
+            String sourceId = soln.getLiteral("source_id").getString();
+            String destId = soln.getLiteral("dest_id").getString();
+            try {
+            	jsonObj.put("startNodeId", sourceId);
+                jsonObj.put("endNodeId", destId);
+				jsonObj.put("edgeLabel", objPropName);
+			}
+            catch (JSONException e) {
+				e.printStackTrace();
+			}
+            edgesArray.put(jsonObj);
         }
         qexec.close();
-		return edgesList;
+		return edgesArray;
 	}
 
 	
@@ -269,7 +231,7 @@ public class TDBDAO {
 	}	
 	
 	
-	public JSONObject generateNodeJSON(Resource resource, Model model) {
+	private JSONObject generateNodeJSON(Resource resource, Model model) {
 		String DCTERMS_NS = "http://purl.org/dc/terms/";
 		JSONObject nodeObj = new JSONObject();
 		try {
--- src/main/java/org/dataone/daks/pbasegsearch/TDBDAOTest.java
+++ src/main/java/org/dataone/daks/pbasegsearch/TDBDAOTest.java
@@ -1,10 +1,13 @@
 package org.dataone.daks.pbasegsearch;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.*;
 
 import com.hp.hpl.jena.query.*;
-import com.hp.hpl.jena.tdb.TDBFactory;
 
 
 public class TDBDAOTest {
@@ -15,10 +18,12 @@ public class TDBDAOTest {
         // Direct way: Make a TDB-back Jena model in the named directory.
         //String directory = "C:\\devp\\apache-tomcat-7.0.50\\bin\\" + args[0];
         String directory = "./" + args[0];
-        Dataset dataset = TDBFactory.createDataset(directory);
+        TDBDAO dao = TDBDAO.getInstance();
+        dao.init(directory);
         TDBDAOTest test = new TDBDAOTest();
         System.out.println("Executing LDBDAOTest with directory: " + directory);
-        test.triplesQuery(dataset);
+        //test.triplesQuery(dao.getDataset());
+        test.getGraphTest(dao);
     }
     
     
@@ -30,6 +35,42 @@ public class TDBDAOTest {
         ResultSetFormatter.out(results);
         qexec.close();
         dataset.close();
+    }
+    
+    
+    private void getGraphTest(TDBDAO dao) {
+    	JSONObject graphObj = dao.getGraph();
+    	String graphJSONStr = graphObj.toString();
+    	System.out.println(graphJSONStr);
+    	this.JSONtoDotFile(graphObj, false, "graphJSONStr.dot");
+    }
+    
+    
+    public void JSONtoDotFile(JSONObject graphObj, boolean btRankdir, String filename) {
+        StringBuilder s = new StringBuilder();
+        String NEWLINE = System.getProperty("line.separator");
+        s.append("digraph dag {" + NEWLINE);
+        if( btRankdir )
+        	s.append("rankdir = BT;" + NEWLINE);
+        try {
+        	JSONArray edgesArray = graphObj.getJSONArray("edges");
+        	for ( int i = 0; i < edgesArray.length(); i++ ) {
+        		JSONObject edgeObj = edgesArray.getJSONObject(i);
+        		String v1 = edgeObj.getString("startNodeId");
+        		String v2 = edgeObj.getString("endNodeId");
+        		s.append("\t" + v1 + " -> " + v2 + ";" + NEWLINE);
+        	}
+        	s.append("}");
+			BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+			writer.write(s.toString());
+			writer.close();
+		}
+        catch (JSONException e) {
+			e.printStackTrace();
+		}
+        catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 	
     
